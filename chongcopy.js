152329aedda3f612df480afda0ff28b62e8469d0
khapham1996 <79965472+khapham1996@users.noreply.github.com>
2021-03-18 21:39:15 +0700

    Update chongcopy.js

--- chongcopy.js
+++ chongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY LIÊN HỆ CHÍNH CHỦ;
+    var pagelink = "<br/><br/>MUON COPY LIÊN HỆ CHÍNH CHỦ";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
